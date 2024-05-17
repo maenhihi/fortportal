2f8557f7f66dc59a986e67c164ac3d50b5f0599b
maenhihi <121367865+maenhihi@users.noreply.github.com>
2024-05-18 00:07:13 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navbar from "./Components/Navbar";
 import MapPage from "./Pages/MapPage";
 import HomePage from "./Pages/HomePage";
-import MapSearchBar from "./Components/MapSearchBar";
-
+import { LogInPage } from "./Pages/LogInPage";
+import ValidLogin from "./Pages/ValidLogin";
+import InvalidLogin from "./Pages/InvalidLogin";
 
 function App() {
   return (
@@ -15,11 +16,13 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/MapPage" element={<MapPage />} />
+            <Route path="/LogInPage" element={<LogInPage />} />
+            <Route path="/ValidLogin" element={<ValidLogin />} />
+            <Route path="/InvalidLogin" element={<InvalidLogin />} />
           </Routes>
         </div>
       </div>
     </Router>
-  
   );
 }
 
